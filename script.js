add98f69b03242d8ffabc01965c3d04205fb8613
hardevkoder <haroldvaldes@yahoo.com>
2023-05-19 17:07:16 -0500

    Agrego boton detener conteo

--- script.js
+++ script.js
@@ -1,70 +1,58 @@
-// Retardo para no interferir con el contenido de la pagina
 setTimeout(() => {
-  // *************************************************
-  // Funcion que ejecuta acciones al presionar boton
-  // *************************************************
-  const presionarBoton = () => {
-    // Desactivo Cursor (elimino foco)
-    txtTiempo.blur();
+  let pausarConteo;
 
-    // Configuro aspecto del Cuadro de estado de la alarma
-    estadoAlarma.style.background = 'green';
-    estadoAlarma.textContent =  'Encendido';
-
-    // Capturo el tiempo a contar
-    const tiempoParada=parseInt(txtTiempo.value)*1000;
-    let tiempo = parseInt(txtTiempo.value);
-
-    conteoDescendente(tiempo, tiempoParada)
-  };
-
-  // **************************************************
-  // Funcion para asignar Enter al boton Activar
-  // **************************************************
+  // Funcion para asignar Enter al boton Start
   const asignarEnter = () => {
     window.addEventListener("keydown", (e) => {
       if (e.key === "Enter") {
-        presionarBoton();
+        btnStartClick();
       }
     });
   };
 
-  // *************************************************
-  // Funcion para Contar descendentemente
-  // *************************************************
-  const conteoDescendente = (tiempo, tiempoParada) => {
-    // Conteo Descendente (flag de detencion en variable)
-    // let detenerConteo = setInterval(() => {
-    //   txtTiempo.value = tiempo;
-    //   tiempo--;
-    // }, 1000);
+  // Funcion que inicia el conteo
+  const iniciarConteo = () => {
+    pausarConteo=false;
+    txtTiempo.blur();                                      // Desactivo Cursor (elimino foco)
+    estadoAlarma.style.background = "green"; 
+    estadoAlarma.textContent = "Conteo Iniciado";
+    tiempoParada = parseInt(txtTiempo.value) * 1000; //Tiempo de Parada
+    let tiempo = parseInt(txtTiempo.value);                //Tiempo Mostrado en pantalla
 
-    // Tiempo para detener conteo
-    setTimeout(() => {
+    setTimeout(() => {                                     //Segundos a Decrementar
       clearInterval(detenerConteo);
-        estadoAlarma.style.background = 'red';
-        estadoAlarma.textContent =  'Apagado';
-    }, tiempoParada+50);
+      // estadoAlarma.style.background = "red";
+      // estadoAlarma.textContent = "Apagado"
+    }, tiempoParada  + 50);
 
-    let detenerConteo = setInterval(() => {
-      tiempo--;
-      txtTiempo.value = tiempo;
-    }, 1000);
-
+    let detenerConteo = setInterval(() => {                //variable para detener conteo           
+        if(!pausarConteo){
+          tiempo--;                                            
+          txtTiempo.value = tiempo;                        //Mostrar Conteo cada segundo (intervalo)
+          if(tiempo===0){
+            estadoAlarma.style.background = "red";
+            estadoAlarma.textContent = "Apagado";
+          }
+        }else{
+          tiempoParada=tiempo;
+          estadoAlarma.style.background = "red";
+          estadoAlarma.textContent = "Conteo Detenido";
+        }
+    }, 1000);                                              
   };
 
-  // *************************************************
-  // ********   PROGRAMA PRINCIPAL   ********
-  // *************************************************
-  // Instanciado de elementos del DOM
-  const txtTiempo = document.getElementById("txtTiempo");
-  const btnActivar = document.getElementById("btnActivar");
-  const estadoAlarma = document.getElementById("estadoAlarma");
-  
-  //Asigno Tecla enter al Boton
-  asignarEnter();
-  
-  // Acciones al Presionar Boton
-  btnActivar.addEventListener('click', presionarBoton);
+  // Funcion que detiene el conteo
+  const detenerConteo = () => {
+    txtTiempo.blur();                                       // Desactivo Cursor (elimino foco)
+    pausarConteo=true;  
+  };
 
-}, 1000);
+  //PROGRAMA PRINCIPAL
+  const txtTiempo = document.getElementById("txtTiempo");       
+  const btnStart = document.getElementById("btnStart");         
+  const btnStop = document.getElementById("btnStop");
+  const estadoAlarma = document.getElementById("estadoAlarma");
+  asignarEnter();
+  btnStart.addEventListener('click',iniciarConteo);
+  btnStop.addEventListener('click',detenerConteo);
+}, 500);
