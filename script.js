edebda0aad8cf48169b94bfa9ecc9fa5d019f62d
hardevkoder <haroldvaldes@yahoo.com>
2023-05-19 20:31:19 -0500

    validacion de tiempo cero

--- script.js
+++ script.js
@@ -4,27 +4,29 @@ setTimeout(() => {
   const txtTiempo = document.getElementById("txtTiempo");
   const btnStop = document.getElementById("btnStop");
   const resultados = document.getElementById("resultados");
-
   //variables Globales
   let cuenta;
   let proceso;
-
+  txtTiempo.value=0;
   //Inicio Conteo
   btnStart.addEventListener("click", () => {
-    resultados.innerHTML="Conteo Iniciado";
-    resultados.style.background="green";
     cuenta = parseInt(txtTiempo.value);
-    proceso = setInterval(() => {
-      cuenta--;
-      txtTiempo.value = cuenta;
-      if(cuenta === 0){
-        clearInterval(proceso);
-        resultados.innerHTML="Conteo Finalizado";
-        resultados.style.background="Red";
-      }
-    }, 1000);
+    if(cuenta===0){
+      alert('Ingresa Tiempo ');
+    }else{
+      resultados.innerHTML="Conteo Iniciado";
+      resultados.style.background="green";
+      proceso = setInterval(() => {
+        cuenta--;
+        txtTiempo.value = cuenta;
+        if(cuenta === 0){
+          clearInterval(proceso);
+          resultados.innerHTML="Conteo Finalizado";
+          resultados.style.background="Red";
+        }
+      }, 1000);
+    }
   });
-
   //Detengo Conteo
   btnStop.addEventListener("click",()=>{
     clearInterval(proceso);
